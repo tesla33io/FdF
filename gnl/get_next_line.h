e00141882345476c669962e1965019ac686fe33a
astavrop <astavrop@student.42berlin.de>
2024-02-13 21:28:28 +0100

    fix: make gnl just a folder.

---
+++ gnl/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 19:00:39 by astavrop          #+#    #+#             */
+/*   Updated: 2024/02/13 16:25:21 by astavrop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif // !BUFFER_SIZE
+
+# include "libft.h"
+
+char		*get_next_line(int fd);
+
+#endif
